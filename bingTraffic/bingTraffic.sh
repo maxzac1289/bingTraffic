a4634ebdf24d7ca3c51b6e7eda1758f7424d9d00
ics45c <mzaccari@uci.edu>
2015-11-22 09:45:25 -0800

    final version

--- bingTraffic/bingTraffic.sh
+++ bingTraffic/bingTraffic.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 bingTraffic(){
-curl http://dev.virtualearth.net/REST/v1/Traffic/Incidents/37,-105,45,-94?key=eqBURiWWTE5afcJ2IqYG~xNlJhANCoR7y-NsuhTEswQ~AtEBP9BiaYkAEDKsgHuJWI0lxmMWjE83qzFw3yUbZdoowdXKaCnaNbJKtUo5eb24 >> $(pwd)/result.json
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+curl http://dev.virtualearth.net/REST/v1/Traffic/Incidents/37,-105,45,-94?key=eqBURiWWTE5afcJ2IqYG~xNlJhANCoR7y-NsuhTEswQ~AtEBP9BiaYkAEDKsgHuJWI0lxmMWjE83qzFw3yUbZdoowdXKaCnaNbJKtUo5eb24 >> $DIR/result.json
 
 }
